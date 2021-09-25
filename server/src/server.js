385aa8fc62afb5b1eea558f973c184f3d2b01b12
Arthur Atul <arthur.atul@ithands.biz>
2021-09-25 20:25:29 +0530

    create server using express

---
+++ server/src/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const app = require('./app');
+const PORT = 8000;
+
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Listening at ${PORT}`);
+})
+
+server.on('error', (err) => {
+    console.log(err);
+})
