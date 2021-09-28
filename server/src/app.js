3ef28566117f5fb5aebc029d55a0339487d6968a
Arthur Atul <arthur.atul@ithands.biz>
2021-09-28 11:49:07 +0530

    add morgan for request logging

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./routes/V1/api');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,6 +10,8 @@ const app = express();
 app.use(cors({
     origin: '*',
 }));
+
+app.use(morgan('combined'));
 
 app.use(express.json());
 
