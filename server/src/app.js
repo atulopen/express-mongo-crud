49a85d84c83ff44c65f897e019b03bce7035a1d3
Arthur Atul <arthur.atul@ithands.biz>
2021-09-28 11:45:22 +0530

    server static files , add deploy command

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = require('./routes/V1/api');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -11,7 +12,13 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use('/v1', router);
+
+app.use('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 
 module.exports = app;
