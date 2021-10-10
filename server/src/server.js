740103e9b55c0a38260869ff51036e2ec45bbd24
Arthur Atul <arthur.atul@ithands.biz>
2021-10-10 13:11:07 +0530

    add https support with self signed signature

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,18 @@
-const http = require('http');
+const https = require('https');
+const fs = require('fs');
 const app = require('./app');
 require('dotenv').config();
 const {connectToMongo} = require("./services/mongo");
 const {loadData} = require("./models/planets.model");
 const {loadLaunchesData} = require("./models/launches.model");
+const path = require("path");
 const PORT = process.env.PORT || 8000;
 
 
-const server = http.createServer(app);
+const server = https.createServer({
+    cert: fs.readFileSync(path.join(__dirname, '..', 'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, '..', 'key.pem')),
+}, app);
 
 
 server.on('error', (err) => {
